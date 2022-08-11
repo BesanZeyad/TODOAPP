04ddf29fba41cc6ddaeb451f3802b7db856f2017
BesanZeyad <107624772+BesanZeyad@users.noreply.github.com>
2022-08-11 14:41:01 +0300

    Modify validation in register and put user data in local storage

--- src/app/register/register/register.component.ts
+++ src/app/register/register/register.component.ts
@@ -22,33 +22,65 @@ export class RegisterComponent  {
  isGenderValid : boolean = true;
  //isEmailValid : boolean = true;
  isPasswordValid : boolean = true;
+ arr : any =[];
+
 
  registeration() : void{
-  console.log(this.firstname);
-  localStorage.setItem("firstname",this.firstname);
-  this.validation();
+  
+  let data = {
+    fname : this.firstname,
+    lastname : this.lastname,
+    age : this.age,
+    gender :this.gender,
+    email : this.email,
+    password :this.password
+  }
 
+  this.arr.push(data);
+  
+  localStorage.setItem("userData",JSON.stringify(this.arr));
+  this.firstname ='';
+  this.lastname ='';
+  this.age = 0; 
+  this.gender ='';
+  this.email= '';
+  this.password='';
+ 
  }
 
  validation():void{
-  if(this.firstname.length<=10 && this.firstname.length >= 30){
-    this.isFirstNameValid=false;
+  if(this.firstname.length>=10 && this.firstname.length <= 30){
+    this.isFirstNameValid=true;
+  }else{
+      this.isFirstNameValid=false;
+    alert('First Name Should be string and between 10 and 30 chars');
+
   }
-  if(this.lastname.length<=10 && this.lastname.length >= 30){
+  if(this.lastname.length>=10 && this.lastname.length <= 30){
+    this.isLastNameValid=true;
+  }else{
+    alert('Second Name Should be string and between 10 and 30 chars');
     this.isLastNameValid=false;
-    alert("First Name Should be string and between 10 and 30 chars");
   }
-  if(this.age<=13 && this.age >= 80){
+  if(this.age>=13 && this.age <= 80){
+    this.isAgeValid=true;
+  }else{
+    alert('Age Should be between 13 and 80');
     this.isAgeValid=false;
   }
   if(!(this.gender == 'male' || this.gender == "female") ){
     this.isGenderValid=false;
+     alert('Gender should be male or female');
     }
-    if(this.password.length<=6 && this.password.length >= 15){
+    if(typeof this.password == 'string' && this.password.length>=6 && this.password.length <= 15){
+      this.isPasswordValid=true;
+    }else{
       this.isPasswordValid=false;
+      alert('Paaword Should be string and between 6 and 15 chars')
     }
-  if(this.isFirstNameValid || this.isLastNameValid || this.isAgeValid || this.isGenderValid){
-    console.log("error");
+  if(this.isFirstNameValid && this.isLastNameValid && this.isAgeValid && this.isGenderValid){
+    this.registeration();
+
   }
  
   this.isEmail(this.email);
